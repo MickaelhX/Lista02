c3178a5cc9f7b21d63bc198c4007dbd9423b1b87
Mickael Henrique Xavier <mickaelhenriquexsilva@gmail.com>
2026-08-28 12:38:01 -0300

    Adiciona exercicios e desafios da Lista 02.

---
+++ Exercicio01.java
@@ -0,0 +1,15 @@
+package Lista02;
+
+public class Exercicio01 {
+    public static void main(String[] args){
+
+        System.out.println("CONTAGEM DOS NUMEROS");
+        int numero = 0;
+
+        while(numero <= 10){
+            System.out.println(numero);
+            numero++;
+        }
+
+    }
+}
